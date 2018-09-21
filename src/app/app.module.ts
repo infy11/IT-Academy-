1bcd2bbd2fbffcd94cdd4462a4dfc3ce73f65315
varnit <varnitgoyal95@gmail.com>
2018-09-22 00:38:03 +0530

    made marks component and basic modification in login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
 import { RegistrationComponent } from './component/registration/registration.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MarksComponent } from './marks/marks.component';
 
 
 const routes: Routes = [
   { path: 'Login', component: LoginComponent},
   { path: 'Registration', component:RegistrationComponent },
+  {path: 'marks',component:MarksComponent},
   { path: 'Navbar', component:NavbarComponent }
 ];
 
@@ -22,11 +24,12 @@ const routes: Routes = [
     LoginComponent,
     RegistrationComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MarksComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
+    RouterModule, 
     NgbModule,
     RouterModule.forRoot(routes)
   ],

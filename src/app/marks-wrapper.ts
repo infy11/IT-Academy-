e234f7960a2bc926c288c5ae66bb404cef8468b6
varnit <varnitgoyal95@gmail.com>
2018-10-02 16:17:17 +0530

    bind marks and marks wrapper class to marks component

---
+++ src/app/marks-wrapper.ts
@@ -0,0 +1,10 @@
+import {Marks} from './marks';
+export class MarksWrapper {
+    constructor(
+   
+        public registrationNum:string,
+        public marks:Marks[],
+       
+         
+      ){}
+}
